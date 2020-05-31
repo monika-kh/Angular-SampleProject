2924af7c066adbb25b75d845265a04014d6ba4ce
monika-kh <khasdevmonika@gmail.com>
2020-05-31 22:38:19 +0530

    New component created.

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+    {
+        name: 'Samsung',
+        price: 1599,
+        description: 'A large phone with one of the best screens'
+    },
+
+    {
+        name: 'IPhone',
+        price: 49000,
+        description: 'A large phone with one of the best cameras'
+    },
+    {
+        name: 'Nokia',
+        price:9799,
+        description: 'Large battry life.'
+    },
+
+    {
+        name: 'Oppo',
+        price:21999,
+        description: 'Good camera quality.'
+    },
+]
